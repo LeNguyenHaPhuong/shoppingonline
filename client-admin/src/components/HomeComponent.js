7d46b665156eabe9a91c9edaf68a355f2a73b614
LeNguyenHaPhuong <151001810+LeNguyenHaPhuong@users.noreply.github.com>
2023-11-28 20:52:47 +0700

    Add files via upload

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN HOME</h2>
+        <img src="https://thaicong.com/wp-content/uploads/2021/12/THAICONG-INDOCHINE-VILLA-By-Wuyhoang01584.jpg" width="800px" height="600px" alt="" />
+      </div>
+    );
+  }
+}
+export default Home;
